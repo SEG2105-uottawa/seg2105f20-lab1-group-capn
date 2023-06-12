7d60f71584c27bbf6787203f7815e73fd8d195b1
Abhay <114037744+SenatorDingo@users.noreply.github.com>
2023-06-12 17:11:19 -0400

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,157 @@
+package alirezamontazer.createbest.productcatalog_v3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    EditText editTextPrice;
+    Button buttonAddProduct;
+    ListView listViewProducts;
+    List<Product> products;
+    DatabaseReference databaseProducts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPrice = (EditText) findViewById(R.id.editTextPrice);
+        listViewProducts = (ListView) findViewById(R.id.listViewProducts);
+        buttonAddProduct = (Button) findViewById(R.id.addButton);
+        databaseProducts = FirebaseDatabase.getInstance().getReference("products");
+
+        products = new ArrayList<>();
+
+
+
+        clickOnAddProduct();
+        onItemLongClick();
+
+
+
+    }
+
+
+    private void clickOnAddProduct() {
+        buttonAddProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addProduct();
+            }
+        });
+    }
+
+    private void onItemLongClick() {
+
+        listViewProducts.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Product product = products.get(i);
+                showUpdateDeleteDialog(product.getId(), product.getProductName());
+                return true;
+            }
+        });
+    }
+
+    private void showUpdateDeleteDialog(final String productId, String productName) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.update_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        final EditText editTextName = (EditText) dialogView.findViewById(R.id.dialog_editTextName);
+        final EditText editTextPrice = (EditText) dialogView.findViewById(R.id.dialog_editTextPrice);
+        final Button buttonUpdate = (Button) dialogView.findViewById(R.id.buttonUpdateProduct);
+        final Button buttonDelete = (Button) dialogView.findViewById(R.id.buttonDeleteProduct);
+
+        dialogBuilder.setTitle(productName);
+        final AlertDialog b = dialogBuilder.create();
+        b.show();
+
+        buttonUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editTextName.getText().toString().trim();
+                double price = Double.parseDouble(editTextPrice.getText().toString());
+                if (!TextUtils.isEmpty(name)) {
+                    updateProduct(productId, name, price);
+                    b.dismiss();
+                }
+            }
+        });
+
+        buttonDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteProduct(productId);
+                b.dismiss();
+            }
+        });
+    }
+
+    private void updateProduct(String id, String name, double price) {
+
+        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+    }
+
+    private void deleteProduct(String id) {
+
+        Toast.makeText(getApplicationContext(), "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+    }
+
+    private void addProduct() {
+
+        Toast.makeText(this, "NOT IMPLEMENTED YET", Toast.LENGTH_LONG).show();
+    }
+
+    protected void onStart(){
+        super.onStart();
+        databaseProducts.addValueEventListener((new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+            public void onCancelled(DatabaseError)
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+
+            }
+        }))
+    }
+
+    private void addProduct(){
+        String id = databaseProducts.push().getKey();
+        Product product = new Product(id,"Joeys Rib Rack", 35);
+        databaseProducts.child(id).se
+
+    }
+
+
+}
